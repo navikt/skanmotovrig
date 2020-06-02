4b369c1827bafb50b6996d9f2650504179f4dd44
erlend-axelsson <erlend.axelsson@gmail.com>
2020-06-02 14:16:41 +0200

    MMA-4515 legger til mdc

--- core/src/main/java/no/nav/skanmotovrig/lagrefildetaljer/STSConsumer.java
+++ core/src/main/java/no/nav/skanmotovrig/lagrefildetaljer/STSConsumer.java
@@ -1,11 +1,9 @@
 package no.nav.skanmotovrig.lagrefildetaljer;
 
 import no.nav.skanmotovrig.config.properties.SkanmotovrigProperties;
-import no.nav.skanmotovrig.constants.MDCConstants;
+import no.nav.skanmotovrig.mdc.MDCConstants;
 import no.nav.skanmotovrig.exceptions.functional.SkanmotovrigFunctionalException;
 import no.nav.skanmotovrig.exceptions.technical.SkanmotovrigTechnicalException;
-import no.nav.skanmotovrig.lagrefildetaljer.data.OpprettJournalpostRequest;
-import no.nav.skanmotovrig.lagrefildetaljer.data.OpprettJournalpostResponse;
 import no.nav.skanmotovrig.lagrefildetaljer.data.STSResponse;
 import no.nav.skanmotovrig.metrics.Metrics;
 import org.slf4j.MDC;
@@ -19,7 +17,6 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 import java.util.Collections;
 
 import static no.nav.skanmotovrig.metrics.MetricLabels.DOK_METRIC;
---
+++ core/src/main/java/no/nav/skanmotovrig/mdc/MDCGenerate.java
@@ -0,0 +1,33 @@
+package no.nav.skanmotovrig.mdc;
+
+import org.slf4j.MDC;
+
+import java.util.UUID;
+
+import static no.nav.skanmotovrig.mdc.MDCConstants.MDC_ZIP_ID;
+import static no.nav.skanmotovrig.mdc.MDCConstants.MDC_NAV_CALL_ID;
+
+public class MDCGenerate {
+
+    public static void generateNewCallIdIfThereAreNone() {
+        if (MDC.get(MDC_NAV_CALL_ID) == null) {
+            MDC.put(MDC_NAV_CALL_ID, UUID.randomUUID().toString());
+        }
+    }
+    public static void clearCallId() {
+        if (MDC.get(MDC_NAV_CALL_ID) != null) {
+            MDC.remove(MDC_NAV_CALL_ID);
+        }
+    }
+    public static void setZipId(String batchId) {
+        MDC.put(MDC_ZIP_ID, batchId);
+    }
+    public static void clearZipId() {
+        if (MDC.get(MDC_ZIP_ID) != null) {
+            MDC.remove(MDC_ZIP_ID);
+        }
+    }
+    public static void clearMdc() {
+        MDC.clear();
+    }
+}
