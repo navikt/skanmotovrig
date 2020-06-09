d624d5efbd3856ad901f8a796ce2e74c676a32a1
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-06-09 15:43:32 +0200

    MMA-3889 La til itest for postboks helse.

---
+++ helse/src/main/java/no/nav/skanmotovrig/helse/HelseConfig.java
@@ -0,0 +1,12 @@
+package no.nav.skanmotovrig.helse;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Joakim Bjørnstad, Jbit AS
+ */
+@Configuration
+@ComponentScan
+public class HelseConfig {
+}
