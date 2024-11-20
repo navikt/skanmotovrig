14c289ba06889736985db03ca5065b2d8ccd9536
Joakim Borgersen <joakim.borgersen@nav.no>
2024-11-20 10:02:58 +0100

    Remove retry mot dokarkiv
    Retry mot dokarkiv førte til at en fil gikk tapt. Etterspør den fra IM og fjerner retry

--- core/src/main/java/no/nav/skanmotovrig/utils/RetryConstants.java
+++ core/src/main/java/no/nav/skanmotovrig/utils/RetryConstants.java
@@ -3,5 +3,4 @@ package no.nav.skanmotovrig.utils;
 public final class RetryConstants {
 	public static final int MAX_RETRIES = 5;
 	public static final int RETRY_DELAY = 500;
-	public static final int MULTIPLIER_SHORT = 2;
 }
