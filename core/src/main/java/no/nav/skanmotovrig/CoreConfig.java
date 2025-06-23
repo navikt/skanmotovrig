63a4e963a6d72f32a9c443676af0d105e78dfe07
Anders Opskar Voldsund <anders.voldsund@nav.no>
2025-06-23 09:41:47 +0200

    MMA-8133 Oppgrader avhengigheiter
    Fiks deprekert spring.codec.max-in-memory-size og bruk av CodecProperties.

--- core/src/main/java/no/nav/skanmotovrig/CoreConfig.java
+++ core/src/main/java/no/nav/skanmotovrig/CoreConfig.java
@@ -3,10 +3,11 @@ package no.nav.skanmotovrig;
 import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import no.nav.dok.jiraapi.JiraProperties;
+import no.nav.dok.jiraapi.JiraProperties.JiraServiceUser;
 import no.nav.dok.jiraapi.JiraService;
-import no.nav.dok.jiraapi.JiraServiceImp;
 import no.nav.dok.jiraapi.client.JiraClient;
 import no.nav.skanmotovrig.config.properties.SkanmotovrigProperties;
+import no.nav.skanmotovrig.config.properties.SkanmotovrigProperties.JiraConfigProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -22,14 +23,15 @@ public class CoreConfig {
 
 	@Bean
 	public JiraService jiraService(JiraClient jiraClient) {
-		return new JiraServiceImp(jiraClient);
+		return new JiraService(jiraClient);
 	}
 
 	@Bean
 	public JiraClient jiraClient(SkanmotovrigProperties properties) {
-		SkanmotovrigProperties.JiraProperties jira = properties.getJira();
+		JiraConfigProperties jira = properties.getJira();
+
 		return new JiraClient(JiraProperties.builder()
-				.jiraServiceUser(new JiraProperties.JiraServiceUser(jira.getUsername(), jira.getPassword()))
+				.jiraServiceUser(new JiraServiceUser(jira.getUsername(), jira.getPassword()))
 				.url(properties.getJira().getUrl())
 				.build());
 	}
